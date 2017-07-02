bf1857416e062aadabb543515acf14a517f56743
Kris Walker <kris@kixx.name>
2017-07-02 19:56:26 -0400

    Implementation with tests
     Changes to be committed:
    	new file:   .eslintrc.js
    	new file:   index.js
    	modified:   package.json
    	new file:   test.js

---
+++ index.js
@@ -0,0 +1,419 @@
+/* globals define, module */
+(function (root, factory) {
+	'use strict';
+	if (typeof define === 'function' && define.amd) {
+		define(['kixx-assert'], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		factory(module.exports);
+	} else {
+		root.kixxAssert = {};
+		factory(root.kixxAssert);
+	}
+}(this, function(exports) {
+	'use strict';
+
+	var hasOwn = Object.prototype.hasOwnProperty;
+	var protoSlice = Array.prototype.slice;
+	var protoToString = Object.prototype.toString;
+
+	function identity(x) {
+		return x;
+	}
+
+	function complement(fn) {
+		return function () {
+			return !fn.apply(this, arguments);
+		};
+	}
+
+	function curry1(fn) {
+		return function f1(a) {
+			if (arguments.length < 1) return f1;
+			return fn(a);
+		};
+	}
+
+	function curry2(fn) {
+		return function f2(a, b) {
+			if (arguments.length < 1) return f2;
+			if (arguments.length < 2) return curry1(function(_b) { return fn(a, _b); });
+			return fn(a, b);
+		};
+	}
+
+	function AssertionError (message, props, caller) {
+		this.message = message || 'Unspecified AssertionError';
+		caller = caller || AssertionError;
+		Error.captureStackTrace(this, caller);
+	}
+
+	AssertionError.prototype = Object.create(Error.prototype);
+
+	AssertionError.prototype.name = 'AssertionError';
+
+	AssertionError.prototype.constructor = AssertionError;
+
+	// Allow errors to be converted to JSON for transfer.
+	AssertionError.prototype.toJSON = function () {
+		var exclude = ['constructor', 'toJSON', 'stack'];
+		var thisKeys = keys(this);
+		var props = {};
+
+		for (var i = thisKeys.length - 1; i >= 0; i--) {
+			var key = thisKeys[i];
+			if (exclude.indexOf(key) === -1) {
+				props[key] = this[key];
+			}
+		}
+
+		this.stack = this.stack;
+		props.name = this.name;
+		return props;
+	};
+
+	function type(x) {
+		if (x === null) return 'Null';
+		if (typeof x === 'undefined') return 'Undefined';
+		return protoToString.call(x).slice(8, -1);
+	}
+
+	function keys(x) {
+		if (x === null) {
+			throw new TypeError('Cannot perform keys() operation on null.');
+		}
+		if (type(x) !== 'Object') {
+			throw new TypeError('Cannot perform keys() operation on a non Object.');
+		}
+
+		var list = [];
+
+		for (var key in x) {
+			if (hasOwn.call(x, key)) list.push(key);
+		}
+
+		return list;
+	}
+
+	function has(key, x) {
+		return hasOwn.call(x, key);
+	}
+
+	function equal(a, b) {
+		if (a === b) {
+			// Make sure 0 !== -0.
+			return a !== 0 || 1 / a === 1 / b;
+		} else {
+			// Make sure NaN === NaN.
+			return a !== a && b !== b;
+		}
+	}
+
+	function greaterThan(a, b) {
+		return b > a;
+	}
+
+	function lessThan(a, b) {
+		return b < a;
+	}
+
+	function match(matcher, x) {
+		if (type(x) !== 'String') return false;
+
+		if (type(matcher) === 'String') return matcher === x;
+
+		return matcher.test(x);
+	}
+
+	function isPrimitive(x) {
+		return x === null || typeof x !== 'object';
+	}
+
+	function isString(x) {
+		return type(x) === 'String';
+	}
+
+	function isNumber(x) {
+		return type(x) === 'Number';
+	}
+
+	function isBoolean(x) {
+		return type(x) === 'Boolean';
+	}
+
+	function isArray(x) {
+		return type(x) === 'Array';
+	}
+
+	function isObject(x) {
+		return type(x) === 'Object';
+	}
+
+	function isNull(x) {
+		return type(x) === 'Null';
+	}
+
+	function isUndefined(x) {
+		return type(x) === 'Undefined';
+	}
+
+	function isNumberNotNaN(x) {
+		return type(x) === 'Number' && !isNaN(x);
+	}
+
+	function isNonEmptyString(x) {
+		return type(x) === 'String' && x.length > 0;
+	}
+
+	function isEmpty(x) {
+		if (isPrimitive(x)) {
+			return !x;
+		}
+
+		switch (type(x)) {
+			case 'Array':
+			case 'String':
+				return x.length === 0;
+			case 'Object':
+				return keys(x).length === 0;
+			default:
+				return Boolean(x);
+		}
+	}
+
+	function includes(item, list) {
+		if (isEmpty(list)) {
+			return false;
+		}
+
+		switch (type(list)) {
+			case 'Array':
+			case 'String':
+				return list.indexOf(item) >= 0;
+			case 'Object':
+				var objectKeys = keys(list);
+				for (var i = objectKeys.length - 1; i >= 0; i--) {
+					if (equal(list[objectKeys[i]], item)) return true;
+				}
+				return false;
+			default:
+				return false;
+		}
+	}
+
+	function toString(x) {
+		switch (type(x)) {
+			case 'String':
+				return 'String("'+ x +'")';
+			case 'Number':
+				return 'Number('+ x +')';
+			case 'Boolean':
+				return 'Boolean('+ x +')';
+			case 'Null':
+				return 'null';
+			case 'Undefined':
+				return 'undefined';
+			case 'Array':
+				if (x.length === 0) {
+					return 'Array([])';
+				}
+				return 'Array([0..'+ (x.length - 1) +'])';
+			case 'Function':
+				if (x.name) {
+					return 'Function('+ x.name +'() {})';
+				}
+				return 'Function(function () {})';
+			case 'Date':
+				return 'Date('+ x.toString() +')';
+			default:
+				if (typeof x.toJSON === 'function') {
+					return JSON.stringify(x.toJSON());
+				}
+				if (typeof x.inspect === 'function') {
+					var rep = x.inspect();
+					if (typeof rep === 'string') {
+						return rep;
+					}
+					return toString(rep);
+				}
+				if (typeof x.constructor === 'function' && x.constructor.name) {
+					if (x.constructor.name === 'Object') {
+						return 'Object({})';
+					}
+					var str = typeof x.toString === 'function' ? x.toString() : '{}';
+					return x.constructor.name +'('+ str +')';
+				}
+				if (typeof x.toString === 'function') {
+					return x.toString();
+				}
+				return protoToString.call(x);
+		}
+	}
+
+	function printObjects() {
+		var objects = new Array(arguments.length);
+		for (var i = 0; i < objects.length; i++) {
+			objects[i] = toString(arguments[i]);
+		}
+		return objects.join(' ');
+	}
+
+	function printf(f) {
+		if (typeof f !== 'string') {
+			return printObjects.apply(this, arguments);
+		}
+
+		if (arguments.length === 1) return f;
+
+		var args = protoSlice.call(arguments, 1);
+		var index = 0;
+		return f.replace(/(%[\w]{1})/g, function () {
+			var str = toString(args[index]);
+			index++;
+			return str;
+		});
+	}
+
+	function composeMessage(message, reason) {
+		if (message) return message + ' :: ' + reason;
+		return reason;
+	}
+
+	function assertion1(guard, reason) {
+		return function innerAssert(a, message) {
+			var result = guard(a);
+
+			if (!result) {
+				throw new AssertionError(
+					composeMessage(message, reason(a)),
+					null,
+					innerAssert
+				);
+			}
+
+			return true;
+		};
+	}
+
+	function assertion2(guard, reason) {
+		return function innerAssert(a, b, message) {
+			var result = guard(a, b);
+
+			if (!result) {
+				throw new AssertionError(
+					composeMessage(message, reason(a, b)),
+					null,
+					innerAssert
+				);
+			}
+
+			return true;
+		};
+	}
+
+	exports.AssertionError = AssertionError;
+
+	exports.helpers = {
+		identity: curry1(identity),
+		complement: curry1(complement),
+		type: curry1(type),
+		keys: curry1(keys),
+		has: curry2(has),
+		equal: curry2(equal),
+		greaterThan: curry2(greaterThan),
+		lessThan: curry2(lessThan),
+		match: curry2(match),
+		isPrimitive: curry1(isPrimitive),
+		isString: curry1(isString),
+		isNumber: curry1(isNumber),
+		isBoolean: curry1(isBoolean),
+		isArray: curry1(isArray),
+		isObject: curry1(isObject),
+		isNull: curry1(isNull),
+		isUndefined: curry1(isUndefined),
+		isDefined: curry1(complement(isUndefined)),
+		isNumberNotNaN: curry1(isNumberNotNaN),
+		isNonEmptyString: curry1(isNonEmptyString),
+		isEmpty: curry1(isEmpty),
+		isNotEmpty: curry1(complement(isEmpty)),
+		includes: curry2(includes),
+		doesNotInclude: curry2(complement(includes)),
+		toString: curry1(toString),
+		printf: printf,
+		assertion1: assertion1,
+		assertion2: assertion2
+	};
+
+	exports.assert = {
+		isOk: assertion1(identity, function (actual) {
+			return printf('expected %x to be truthy', actual);
+		}),
+
+		isNotOk: assertion1(complement(identity), function (actual) {
+			return printf('expected %x not to be truthy', actual);
+		}),
+
+		isEqual: assertion2(equal, function (expected, actual) {
+			return printf('expected %x to equal %x', actual, expected);
+		}),
+
+		isNotEqual: assertion2(complement(equal), function (expected, actual) {
+			return printf('expected %x not to equal %x', actual, expected);
+		}),
+
+		isMatch: assertion2(match, function (pattern, actual) {
+			return printf('expected %x to match %x', actual, pattern);
+		}),
+
+		isNotMatch: assertion2(complement(match), function (pattern, actual) {
+			return printf('expected %x not to match %x', actual, pattern);
+		}),
+
+		isUndefined: assertion1(isUndefined, function (actual) {
+			return printf('expected %x to be undefined', actual);
+		}),
+
+		isDefined: assertion1(complement(isUndefined), function (actual) {
+			return printf('expected %x to be defined', actual);
+		}),
+
+		isEmpty: assertion1(isEmpty, function (actual) {
+			return printf('expected %x to be empty', actual);
+		}),
+
+		isNotEmpty: assertion1(complement(isEmpty), function (actual) {
+			return printf('expected %x not to be empty', actual);
+		}),
+
+		includes: assertion2(includes, function (item, list) {
+			return printf('expected %x to include %x', list, item);
+		}),
+
+		doesNotInclude: assertion2(complement(includes), function (item, list) {
+			return printf('expected %x not to include %x', list, item);
+		}),
+
+		has: assertion2(has, function (key, target) {
+			return printf('expected %x to have key %x', target, key);
+		}),
+
+		doesNotHave: assertion2(complement(has), function (key, target) {
+			return printf('expected %x not to have key %x', target, key);
+		}),
+
+		isGreaterThan: assertion2(greaterThan, function (base, actual) {
+			return printf('expected %x to be greater than %x', actual, base);
+		}),
+
+		isLessThan: assertion2(lessThan, function (base, actual) {
+			return printf('expected %x to be less than %x', actual, base);
+		}),
+
+		isNonEmptyString: assertion1(isNonEmptyString, function (actual) {
+			return printf('expected %x to be a non empty String', actual);
+		}),
+
+		isNumberNotNaN: assertion1(isNumberNotNaN, function (actual) {
+			return printf('expected %x to be a number and not NaN', actual);
+		})
+	};
+}));
---
+++ test.js
@@ -0,0 +1,515 @@
+/* globals require, kixxAssert, console */
+/* eslint no-console: "off" */
+(function (kixxAssert) {
+	'use strict';
+
+	var helpers = kixxAssert.helpers;
+	var assert = kixxAssert.assert;
+	var AssertionError = kixxAssert.AssertionError;
+
+	function test(title, block) {
+		try {
+			block();
+		} catch (err) {
+			console.error('In: '+ title);
+			console.error(err.stack);
+			return false;
+		}
+		return true;
+	}
+
+	test('AssertionError', function () {
+		var err = new AssertionError();
+		assert.isEqual('Unspecified AssertionError', err.message, '.message');
+		assert.isEqual('AssertionError', err.name, '.name');
+
+		// Slices unwanted items off the stack when capturing the stack trace.
+		// The first line of the stack trace is .name and .message, the second
+		// line is a reference to the file location, which should be right here:
+		var secondLine = err.stack.split('\n')[1];
+		assert.isMatch(/test\.js:[\d]{2}:[\d]{2}$/, secondLine, '.stack');
+	});
+
+	test('helpers.identity()', function () {
+		var x = {rnd: Math.random()};
+		var y = helpers.identity(x);
+		assert.isEqual(x, y, 'returns exact given argument');
+	});
+
+	test('helpers.complement()', function () {
+		var flipflop = helpers.complement(helpers.identity);
+		var x = true;
+		var y = flipflop(x);
+		assert.isEqual(false, y, 'creates a function which returns the boolean opposite');
+	});
+
+	test('helpers.toString()', function () {
+		var s = '';
+		var i = 0;
+		var b = false;
+		var f = 1.1;
+		var u = (function (_a) { return _a; }());
+		var n = null;
+		var nn = 1 / 'foo'; // NaN
+		var a = [null, null];
+		var o = {};
+		var d = new Date();
+		var r = /foo$/;
+
+		var fn = function boilWater() {};
+
+		var json = {
+			toJSON: function () {
+				return {foo: 'bar'};
+			}
+		};
+
+		function Cat() {}
+		Cat.prototype.toString = function () { return 'Garfield'; };
+
+		var cat = new Cat();
+
+		assert.isEqual('String("")', helpers.toString(s), 'String');
+		assert.isEqual('Number(0)', helpers.toString(i), 'Number');
+		assert.isEqual('Boolean(false)', helpers.toString(b), 'Boolean');
+		assert.isEqual('Number(1.1)', helpers.toString(f), 'Float');
+		assert.isEqual('undefined', helpers.toString(u), 'Undefined');
+		assert.isEqual('null', helpers.toString(n), 'Null');
+		assert.isEqual('Number(NaN)', helpers.toString(nn), 'NaN');
+		assert.isEqual('Array([0..1])', helpers.toString(a), 'Array');
+		assert.isEqual('Object({})', helpers.toString(o), 'Object');
+		assert.isEqual('Date('+ d.toString() +')', helpers.toString(d), 'Date');
+		assert.isEqual('RegExp(/foo$/)', helpers.toString(r), 'RegExp');
+		assert.isEqual('Function(boilWater() {})', helpers.toString(fn), 'function');
+		assert.isEqual('{"foo":"bar"}', helpers.toString(json), 'with toJSON');
+		assert.isEqual('Cat(Garfield)', helpers.toString(cat), 'custom constructor');
+	});
+
+	test('assert.isOk()', function () {
+		try {
+			assert.isOk(false, 'should fail');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('should fail :: expected Boolean(false) to be truthy', err.message);
+		}
+
+		assert.isOk(true, 'should pass');
+	});
+
+	test('assert.isNotOk()', function () {
+		try {
+			assert.isNotOk(true, 'should fail');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('should fail :: expected Boolean(true) not to be truthy', err.message);
+		}
+
+		assert.isNotOk(false, 'should pass');
+	});
+
+	test('assert.isEqual()', function () {
+		try {
+			assert.isEqual({}, {}, 'should fail');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('should fail :: expected Object({}) to equal Object({})', err.message);
+		}
+	});
+
+	test('assert.isNotEqual()', function () {
+		var x = {};
+		var y = x;
+
+		try {
+			assert.isNotEqual(x, y, 'should fail');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('should fail :: expected Object({}) not to equal Object({})', err.message);
+		}
+
+		assert.isNotEqual({}, {}, 'should pass');
+	});
+
+	test('assert.isMatch()', function () {
+		assert.isMatch('foo', 'foo', 'using string match');
+		assert.isMatch(/^[fo]{3}$/, 'foo', 'using RegExp match');
+	});
+
+	test('assert.isDefined', function () {
+		var obj = {
+			x: null
+		};
+
+		try {
+			assert.isDefined(obj.y, 'fails');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('fails :: expected undefined to be defined', err.message);
+		}
+
+		assert.isDefined(obj.x, 'passes');
+	});
+
+	test('assert.isNotEmpty', function () {
+		var s = '';
+		var i = 0;
+		var b = false;
+		var u = (function (_a) { return _a; }());
+		var n = null;
+		var nn = 1 / 'foo'; // NaN
+		var a = [0, 0];
+		var o = {};
+		var d = new Date();
+
+		var fn = function boilWater() {};
+
+		function Cat() {
+			this.color = 'black';
+		}
+
+		var cat = new Cat();
+
+		try {
+			assert.isNotEmpty(s, 'String');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.isNotEmpty('foo', 'full String');
+
+		try {
+			assert.isNotEmpty(i, 'Integer');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.isNotEmpty(-1, 'truthy Number');
+
+		try {
+			assert.isNotEmpty(b, 'Boolean');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.isNotEmpty(true, 'true');
+
+		try {
+			assert.isNotEmpty(u, 'undefined');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isNotEmpty(n, 'null');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isNotEmpty(nn, 'NaN');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.isNotEmpty(a, 'Array');
+
+		try {
+			assert.isNotEmpty(o, 'Object');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isNotEmpty(d, 'Date');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isNotEmpty(fn, 'Function');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.isNotEmpty(cat, 'Cat');
+	});
+
+	test('assert.includes()', function () {
+		var s = '';
+		var i = 1;
+		var b = true;
+		var u = (function (_a) { return _a; }());
+		var n = null;
+		var nn = 1 / 'foo'; // NaN
+		var d = new Date();
+
+		var fn = function boilWater() {};
+
+		var x = {};
+
+		var a = [null, x, null];
+		var o = {a: null, b: x, c: null};
+		var str = 'The quick brown fox jumped over the lazy dog.';
+
+		try {
+			assert.includes('', s, 'String');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.includes(1, i, 'Integer');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.includes(true, b, 'Integer');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.includes(u, u, 'undefined');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.includes(null, n, 'null');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.includes(NaN, nn, 'NaN');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.includes(d.getMonth(), d, 'Date');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.includes(0, fn, 'Function');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.includes(x, a, 'Array');
+		assert.includes(x, o, 'Object');
+		assert.includes('fox', str, 'non empty String');
+	});
+
+	test('assert.has()', function () {
+		var u = (function (_a) { return _a; }());
+		var o = {x: u};
+		var a = [u];
+
+		assert.has('x', o, 'Object key');
+		assert.has('0', a, 'Arry string key');
+		assert.has(0, a, 'Arry integer index');
+
+		try {
+			assert.has('k', u, 'undefined list');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('TypeError', err.name);
+		}
+
+		try {
+			assert.has('k', null, 'null list');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('TypeError', err.name);
+		}
+
+		try {
+			assert.has('k', 'k', 'string');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.has(1, 1, 'number');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		// Auto assigned members.
+		assert.has('length', a, '[].length');
+		assert.has('length', 'foo', '"foo".length');
+	});
+
+	test('assert.doesNotHave()', function () {
+		var u = (function (_a) { return _a; }());
+		var o = {x: u};
+		var a = [u];
+		var d = new Date();
+
+		try {
+			assert.doesNotHave('x', o, 'Object key fail');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.doesNotHave('k', 0, 'Object key pass');
+
+		try {
+			assert.doesNotHave('0', a, 'Array key string fail');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.doesNotHave(0, a, 'Array index integer fail');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.doesNotHave('1', a, 'Array key string pass');
+		assert.doesNotHave(1, a, 'Array index integer pass');
+
+		// Prototype members
+		assert.doesNotHave('toString', d, 'toString');
+		assert.doesNotHave('getMonth', d, 'getMonth');
+	});
+
+	test('assert.isGreaterThan()', function () {
+		var u = (function (_a) { return _a; }());
+
+		assert.isGreaterThan(0, 1, 'integer');
+		assert.isGreaterThan(0.1, 0.2, 'float');
+		assert.isGreaterThan('a', 'b', 'letter');
+		assert.isGreaterThan('A', 'a', 'case sensitive letter');
+
+		try {
+			assert.isGreaterThan(1, 1, 'same integer');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isGreaterThan('a', 'a', 'same letter');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isGreaterThan(null, u, 'null undefined');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+	});
+
+	test('assert.isLessThan()', function () {
+		var u = (function (_a) { return _a; }());
+
+		assert.isLessThan(1, 0, 'integer');
+		assert.isLessThan(0.2, 0.1, 'float');
+		assert.isLessThan('b', 'a', 'letter');
+		assert.isLessThan('a', 'A', 'case sensitive letter');
+
+		try {
+			assert.isLessThan(1, 1, 'same integer');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isLessThan('a', 'a', 'same letter');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isLessThan(null, u, 'null undefined');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+	});
+
+	test('assert.isNonEmptyString', function () {
+		var a = [0, 1];
+		var x = 'xxx';
+		var y = '';
+
+		try {
+			assert.isNonEmptyString(a, 'Array');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isNonEmptyString(y, 'empty string');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.isNonEmptyString(x, 'passing');
+	});
+
+	test('assert.isNumberNotNaN', function () {
+		var a = [0, 1];
+		var s = '1';
+		var nn = 1 / 'foo'; // NaN
+
+		try {
+			assert.isNumberNotNaN(a, 'Array');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isNumberNotNaN(s, 'String');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		try {
+			assert.isNumberNotNaN(nn, 'NaN');
+			throw new AssertionError('Should have thrown!');
+		} catch (err) {
+			assert.isEqual('AssertionError', err.name);
+		}
+
+		assert.isNumberNotNaN(0, 'zero');
+		assert.isNumberNotNaN(1.1, 'float');
+		assert.isNumberNotNaN(Infinity, 'Infinity');
+	});
+}(typeof require === 'undefined' ? kixxAssert : require('./')));
