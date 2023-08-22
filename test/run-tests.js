4b871509d8329fa7868fe0be89ebcbaf3a294339
Kris Walker <kristoffer.walker@disneystreaming.com>
2023-08-22 08:17:47 -0400

    Add tests for isEqual()
     Changes to be committed:
    	modified:   test/assert-equal-test.js
    	new file:   test/is-equal-test.js
    	modified:   test/run-tests.js

--- test/run-tests.js
+++ test/run-tests.js
@@ -1,16 +1,18 @@
-import test_assertThrowsErrorMessage from './assert-throws-error-message-test.js';
+import test_isEqual from './is-equal-test.js';
 import test_assert from './assert-test.js';
 import test_assertFalsy from './assert-falsy-test.js';
 import test_assertEqual from './assert-equal-test.js';
 import test_assertNotEqual from './assert-not-equal-test.js';
 import test_assertMatches from './assert-matches-test.js';
 import test_assertNotMatches from './assert-not-matches-test.js';
+import test_assertThrowsErrorMessage from './assert-throws-error-message-test.js';
 
 
-test_assertThrowsErrorMessage();
+test_isEqual();
 test_assert();
 test_assertFalsy();
 test_assertEqual();
 test_assertNotEqual();
 test_assertMatches();
 test_assertNotMatches();
+test_assertThrowsErrorMessage();
