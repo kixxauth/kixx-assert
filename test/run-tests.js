b13cd29070edd6d68f6b3182526b72caa8e02c71
Kris Walker <kristoffer.walker@disneystreaming.com>
2023-08-22 06:00:55 -0400

    Added tests for assertNotEqual()
     Changes to be committed:
    	new file:   test/assert-not-equal-test.js
    	modified:   test/run-tests.js

--- test/run-tests.js
+++ test/run-tests.js
@@ -1,9 +1,11 @@
 import test_assertThrowsErrorMessage from './assert-throws-error-message-test.js';
 import test_assert from './assert-test.js';
 import test_assertFalsy from './assert-falsy-test.js';
-import tests_assertEqual from './assert-equal-test.js';
+import test_assertEqual from './assert-equal-test.js';
+import test_assertNotEqual from './assert-not-equal-test.js';
 
 test_assertThrowsErrorMessage();
 test_assert();
 test_assertFalsy();
-tests_assertEqual();
+test_assertEqual();
+test_assertNotEqual();
